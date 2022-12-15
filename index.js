1984ebbf714135241d27e2084746750821ea3326
oguzhanural <oguzhnural@gmail.com>
2022-12-15 11:27:28 +0300

    added github

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DBConnection is Successful! "))
+.catch((err) => {
+    console.log(err);
+});
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5500, () =>{
+    console.log("Beckend server is running...");
+});
